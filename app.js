9f0569ac135d0daec413d492202d30190231913b
root <root@server-netvista.aldomoro.org>
2016-12-01 20:25:03 +0100

    b

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //
 // Shannon Game using HTML5 and JavaScript
-// 08/11/2016, Giulio Zausa
+// 08/11/2016, Giulio Zausa, Leonardo Pinton
 //
 
 // Hardcoded JSONs URLs
@@ -14,19 +14,26 @@ var 	info1Bit	= Math.log2(2);
 
 // DOM Elements
 var outputSpan  = document.getElementById("output"),
-    errorsSpan  = document.getElementById("errors"),
-    recSpan     = document.getElementById("rec"),
+    errori      = document.getElementById("errori"),
+    percErrors	= document.getElementById("percErrors"),
+    giuste		= document.getElementById("giuste"),
+    rindondanza = document.getElementById("rindondanza"),
     moderrSpan  = document.getElementById("modalerr"),
     nameInput   = document.getElementById("nameInput"),
     dropd       = document.getElementById("dropd"),
     hiTable     = document.getElementById("hiTable");
-
+	
 // Global Variables
-var currentText, currentIndex, currentErrors, currentL, currentSubject, currentInfoS, currentInfoT;
+var currentText, currentIndex, currentErrors, currentLength, currentSubject, currentInfoS, currentInfoT, currentGuesses;
 
 // Populate Lists
 PopulateDropDown();
 PopulateHiScores();
+
+// TESTO CIFRATO button Click
+document.getElementById("testoCifratoBut").addEventListener("click", function(){
+    outputSpan.innerText = ErrSentence(currentText, currentGuesses);
+});
 
 // Keyboard Hook
 RegisterCharEvent(function(key) {
@@ -35,38 +42,43 @@ RegisterCharEvent(function(key) {
     {
         // Checks for error
         if (key.toLowerCase() != removeAccents(currentText.substring(currentIndex, currentIndex + 1).toLowerCase())) {
+            // -Error-
             currentErrors++;
-            errorsSpan.innerText = currentErrors;
-            recSpan.innerText = currentInfoT - currentInfoS;
+            errori.innerText = currentErrors;
         }
-		currentInfoS = (currentL - currentErrors) * info1Bit + currentErrors * infoPerChar;
-		recSpan.innerText = currentInfoT - currentInfoS;
-        
-        // Errors colors
-        if (currentErrors == 0) {
-            errorsSpan.className = "text-success";
-        }
-        else if (currentErrors < 5) {
-            errorsSpan.className = "text-warning";
-        }
-        else {
-            errorsSpan.className = "text-danger";
+		else {
+            // -Guess-
+            currentGuesses++;
+            giuste.innerText = currentGuesses;
         }
         
-        // Update output
+        // Update output with another character
         currentIndex++;
         outputSpan.innerText = RenderGrayedText(currentText, "\u2022", currentIndex);
         
         // Checks if next character is a letter
-        while (currentIndex < currentText.length && !IsCharOrSpace(removeAccents(currentText.substring(currentIndex, currentIndex + 1)).toUpperCase().charCodeAt(0)))
+        while (currentIndex < currentText.length &&
+                !IsCharOrSpace(removeAccents(currentText.substring(currentIndex, currentIndex + 1)).toUpperCase().charCodeAt(0)))
         {
             currentIndex++;
             outputSpan.innerText = RenderGrayedText(currentText, "\u2022", currentIndex);
-            currentL--;
-			currentInfoT = currentL * infoPerChar;
+
+            // Update statistics with new length
+            currentLength--;
+			currentInfoT = currentLength * infoPerChar;
         }
+
+        // -- STATISTICS --
+        // Percent error
+        var percent = (Math.round((currentErrors / currentLength) * 100000)) / 1000;
+		percErrors.innerText = "(" + percent + "%)";
+
+        // Information and Rendundancy
+		currentInfoS = currentGuesses * info1Bit + currentErrors * infoPerChar;
+        var rendundancy = (Math.round((currentInfoT - currentInfoS) * 1000)) / 1000;
+		rindondanza.innerText = rendundancy + " bit";
         
-        // Win Modal
+        // Win Modal - SAVE HIGHSCORE
         if (currentIndex == currentText.length) {
             moderrSpan.innerText = currentErrors;
             $('#hsModal').modal();
@@ -78,11 +90,11 @@ RegisterCharEvent(function(key) {
 function saveClick()
 {
     var nameToSave = nameInput.value;
-	currentInfoS = (currentL - currentErrors) * info1Bit + currentErrors * infoPerChar;
-	currentInfoT = currentL * infoPerChar;
+	currentInfoS = currentGuesses * info1Bit + currentErrors * infoPerChar;
+	currentInfoT = currentLength * infoPerChar;
     var rec = currentInfoT - currentInfoS;
     loadJSON(highScoreURL, function(data){
-        data.push({"name": nameToSave, "err": currentErrors, "rec": rec, "l": currentL, "subject": subjects[currentSubject]});
+        data.push({"name": nameToSave, "err": currentErrors, "rec": rec, "l": currentLength, "subject": subjects[currentSubject]});
         saveJSON(highScoreURL, data, function () {
             PopulateHiScores();
             $('#hsModal').modal('hide');
@@ -98,12 +110,14 @@ function newGame(subject)
     currentText = GetRandomSentence(texts[subject][randomIntFromInterval(0, texts[subject].length)], nwords).trim();
     currentIndex = 0;
     currentErrors = 0;
-    currentL = currentText.length;
-	currentInfoT = currentL * infoPerChar;
-	currentInfoS = (currentL - currentErrors) * info1Bit + currentErrors * infoPerChar;
+    currentGuesses = 0;
+    currentLength = currentText.length;
+	currentInfoT = currentLength * infoPerChar;
+	currentInfoS = currentGuesses * info1Bit + currentErrors * infoPerChar;
     outputSpan.innerText = RenderGrayedText(currentText, "\u2022", currentIndex);
 }
 
+// Adds elements to the dropdown start game menu
 function PopulateDropDown()
 {
     for (var i = 0; i < subjects.length; i++)
@@ -112,29 +126,31 @@ function PopulateDropDown()
     }
 }
 
+// Fills the Highscores table
 function PopulateHiScores()
 {
     // Loads the JSON
     loadJSON(highScoreURL, function(data){
         // Sort the table by rendundancy
-        data.sort(function(a,b) {return (a.rec > b.rec) ? 1 : ((b.rec > a.rec) ? -1 : 0);} );
+        data.sort(function(a,b) {return (a.rec < b.rec) ? 1 : ((b.rec < a.rec) ? -1 : 0);} );
         
         // Populate the table
         var html = "";
         for (var i = 0; i < data.length; i++)
         {
             html += "<tr>";
-            html += "<th scope=\"row\">" + i + "</td>";
-            html += "<td>" + data[i].name + "</td>";
-            html += "<td>" + data[i].subject + "</td>";
-            html += "<td>" + data[i].err + "</td>";
-            html += "<td>" + data[i].rec + "</td>";
+                html += "<th scope=\"row\">" + i + "</td>";
+                html += "<td>" + data[i].name + "</td>";
+                html += "<td>" + data[i].subject + "</td>";
+                html += "<td>" + data[i].err + "</td>";
+                html += "<td>" + data[i].rec + "</td>";
             html += "</tr>";
         }
         hiTable.innerHTML = html;
     });
 }
 
+// For internal use
 function ResetHiScores()
 {
     loadJSON(highScoreURL, function(data){
